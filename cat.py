8a8a529f51934201d4cac45c8ad425a99baa3fd8
parthivparthivparthivparthivparthivpart <parthiv.chowdhury@laetottenham.ac.uk>
2024-11-05 23:25:58 +0000

    finished kinda

--- cat.py
+++ cat.py
@@ -1,3 +1,4 @@
+import random
 class Cat:
     # the constructor will create a cat for us in code
     # to create a cat we need given_name and given_colour
@@ -11,35 +12,144 @@ class Cat:
         self.weight = 5
         self.combat = 1
         self.money = 0
-    
+        self.sanity = 100
+
     def train(self):
         print(f"{self.name} is doing push-ups...")
         self.energy -= 5
         self.intelligence += 2
         self.age += 0.1
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
     def feed(self):
         print(f"{self.name} ate some freshly cooked ravioli...")
         self.energy += 10
         self.weight += 1
         self.age += 0.1
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
     def play(self):
         print(f"{self.name} played with the toy you handed to them")
         self.energy -= 5
         self.weight -= 0.5
         self.age += 0.1
         self.intelligence -= 2
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
     def sleep(self):
         print(f"You tucked {self.name} in its bed and destroyed each enemy that dare attack")
         self.energy += 15
         self.age += 0.1
         self.combat += 0.5
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
     def fight(self):
         print(f"you and {self.name} both fought valiantly against shadow warriors of Sir Catkiller IV, and you stole some money")
         self.energy -= 10
         self.combat += 1
         self.money += 5
+        self.age += 0.1
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
 
-
+class Eldritch_God:
+    def __init__(self, given_name, given_colour):
+        self.name = given_name
+        self.colour = given_colour
+        self.age = 1
+        self.energy = 60
+        self.intelligence = 10
+        self.weight = 10
+        self.combat = 10
+        self.money = 0
+        self.sanity = -100
+    def train(self):
+        print(f"{self.name} is doing push-ups...")
+        self.energy -= 5
+        self.intelligence += 2
+        self.age += 0.1
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
+    def feed(self):
+        print(f"{self.name} ate some freshly cooked ravioli...")
+        self.energy += 10
+        self.weight += 1
+        self.age += 0.1
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
+    def play(self):
+        print(f"{self.name} played with the toy you handed to them")
+        self.energy -= 5
+        self.weight -= 0.5
+        self.age += 0.1
+        self.intelligence -= 2
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
+    def sleep(self):
+        print(f"You tucked {self.name} in its bed and destroyed each enemy that dare attack")
+        self.energy += 15
+        self.age += 0.1
+        self.combat += 0.5
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
+    def fight(self):
+        print(f"you and {self.name} both fought valiantly against shadow warriors of Sir Catkiller IV, and you stole some money")
+        self.energy -= 10
+        self.combat += 1
+        self.money += 5
+        self.age += 0.1
+        chance = random.randint(1, 3)
+        if chance == 2:
+            self.sanity += 10
+        if chance == 1:
+            self.sanity -= 10
+        if chance == 3:
+            self.sanity += 0
     
     
 
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from cat import Cat
-
+from cat import Eldritch_God
+import re
+import random
 print("Welcome to Parthiv Dev Chowdhury's game of cats")
-
+def stats():
+    print(f"""Age: {my_cat.age}
+Intelligence: {my_cat.intelligence}
+Weight: {my_cat.weight}
+Energy: {my_cat.energy}
+combat: {my_cat.combat}
+sanity: {my_cat.sanity}
+Money: {my_cat.money}
+""") 
+def miracle():
+     a = random.randint(1, 10)
+     if a == 7:
+          my_cat.weight += 1
+          my_cat.intelligence += 1
+          my_cat.energy += 15
+          my_cat.combat += 2
+          my_cat.sanity += 10
+          my_cat.money += 100
 name = input("Enter the cat's name: ")
 colour = input("enter thy cats colour: ")
-my_cat = Cat(name, colour)
-while True:
-    action = input("""
+if re.search(r'[^a-zA-Z0-9]', name):
+    print("We have deduced that your cat is actually an eldritch God")
+    while True:
+        
+        my_cat = Eldritch_God(name, colour)
+        action = input("""
 What would you like to do:
 1. Train intelligence
 2. Feed
@@ -14,11 +36,56 @@ What would you like to do:
 4. Sleep and defend
 5. Slay enemies
 """)
-    
-    if action == "1":
-        my_cat.train()
-    if action == "2":
-        my_cat.feed()
-    if action == "3":
-        my_cat.play()
-    
+        if action == "1":
+            my_cat.train()
+            stats()
+            miracle()
+        if action == "2":
+            my_cat.feed()
+            stats()
+            miracle()
+        if action == "3":
+            my_cat.play()
+            stats()
+            miracle()
+        if action == "4":
+            my_cat.sleep()
+            stats()
+            miracle()
+        if action == "5":
+            my_cat.fight()
+            stats()
+            miracle()
+        if my_cat.sanity > 0:
+            print("Your cat, also known as Azathoth, has now fully awoken and had destroyed the known universe")
+if name.isalpha():
+    my_cat = Cat(name, colour)
+    while True:
+        action = input("""
+What would you like to do:
+1. Train intelligence
+2. Feed
+3. Play
+4. Sleep and defend
+5. Slay enemies
+""")
+
+        if action == "1":
+            my_cat.train()
+            stats()
+        if action == "2":
+            my_cat.feed()
+            stats()
+        if action == "3":
+            my_cat.play()
+            stats()
+        if action == "4":
+            my_cat.sleep()
+            stats()
+        if action == "5":
+            my_cat.fight()
+            stats()
+        if my_cat.sanity < 0:
+             print("Your cat becomes insane and suddenly enlarges, and eats you")
+             break
+
